58dfb93e54ac0e01c18d3a8a88c9302e617e2034
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-13 20:34:50 +0530

    Time: 1 ms (99.07%) | Memory: 42.7 MB (32.52%) - LeetSync

---
+++ 263-ugly-number/ugly-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0) return false;
+        int fact[] = {2,3,5};
+        for(int num : fact){
+            while(n % num == 0)
+                n = n / num;
+        }
+        return n == 1;
+    }
+}
