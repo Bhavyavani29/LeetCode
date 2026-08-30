9581087162e2bc70ad87f77276bc1990a176e346
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-30 21:59:05 +0530

    Time: 2 ms (98.03%) | Memory: 72.5 MB (30.21%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int n = bills.length;
+        int c5 = 0, c10 = 0, c20 = 0;
+		for(int i = 0;i < n;i++){
+			if(bills[i] == 5)
+				c5++;
+			else if(bills[i] == 10){
+				if(c5 == 0)
+					return false;
+				c5--;
+				c10++;
+			}
+			else if(bills[i] == 20){
+				if(c10 > 0 && c5 > 0){
+					c10--;
+					c5--;
+				}
+				else if(c5 >= 3){
+					c5 -= 3;
+				}
+				else{
+					return false;
+				}
+			}
+		}
+		return true;
+    }
+}
